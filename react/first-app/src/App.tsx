babfcf5fcb13cb8e0a7dd4f6ed1132d229105491
joonamin <joonamin44@gmail.com>
2025-01-10 05:29:35 +0900

    react first app

---
+++ react/first-app/src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="main">
+        <header>
+          <img src="/src/assets/react.svg" alt="react-logo" />
+          <h1>React Essentials</h1>
+        </header>
+        
+        <p className="content">
+          Fundamental React concepts you will need for almost any app you are going to build!
+        </p>
+
+        <p className="sub-content">
+          Time to get started
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App

2f072fdeff2fe2d648e3e29bd1173a30b9c94d6f
joonamin <joonamin44@gmail.com>
2025-01-15 22:05:53 +0900

    forwarded prop과 proxy prop을 구분하여 자식 요소들에게 prop 전달하기

--- react/deep-dive/src/components/Section.jsx
+++ react/deep-dive/src/components/Section.jsx
@@ -1,6 +1,7 @@
-export default function Section({ id, title, children }) {
+// Rest property 를 사용하여 남은 [key: value] pair에 대해서 속성을 그룹화하여 한번에 가져올 수 있다.
+export default function Section({ title, children, ...props }) {
     return (
-        <section id={id}>
+        <section {...props}>
             <h2>{title}</h2>
             {children}
         </section>
